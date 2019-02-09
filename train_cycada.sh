faaed67f654c58fb98680aaacfbbd9746611fda1
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-02-09 14:38:36 +0000

    change data dir

--- train_cycada.sh
+++ train_cycada.sh
@@ -4,6 +4,6 @@ CUDA_VISIBLE_DEVICES=1 python train.py --name cycada_svhn2mnist_noIdentity \
     --model cycle_gan_semantic \
     --lambda_A 1 --lambda_B 1 --lambda_identity 0 \
     --no_flip --batchSize 100 \
-    --dataset_mode mnist_svhn --dataroot /x/jhoffman/ \
+    --dataset_mode mnist_svhn --dataroot data \
     --which_direction BtoA
 
